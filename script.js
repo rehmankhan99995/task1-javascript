01a78c96b3a6b35a80ba9a5a87b2ddef81a5147f
rehman <rehman00004khan@gmail.com>
2025-06-08 15:43:12 +0500

    task 1 done

--- script.js
+++ script.js
@@ -9,18 +9,26 @@
 
 // }
 
-let input = prompt("").toLowerCase();  // convert to lowercase
-let spaceIndex = input.indexOf(" ");       // find the space position
+// let input = prompt("").toLowerCase();  // convert to lowercase
+// let spaceIndex = input.indexOf(" ");       // find the space position
 
-// First word
-for (let i = 0; i < spaceIndex; i++) {
-  console.log(input[i]);
-}
+// // First word
+// for (let i = 0; i < spaceIndex; i++) {
+//   console.log(input[i]);
+// }
 
-console.log(""); // print a blank line between words
+// console.log(""); // print a blank line between words
 
 // Second word — only first 4 characters
 // for (let i = spaceIndex + 1; i < spaceIndex + 5; i++) {
 //   console.log(input[i]);
 // }
 
+// task 1 
+
+var askname = prompt('')
+  for (let i = 0; i < askname.length; i++) {
+    console.log(askname[i]);
+    
+  }
+//  var b = alert(askname)
